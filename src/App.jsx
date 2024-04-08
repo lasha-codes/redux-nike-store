629490bf99304d3fe185f0dc5e338e52ceda41da
Lasha Magaldadze <lashagiorgi420@gmail.com>
2024-04-08 16:04:21 -0700

    adding a structure of our Hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import { Hero } from './components'
+
 const App = () => {
   return (
     <>
-      <h1 className='bg-black text-white text-4xl font-bold'>
-        Welcome to Nike store e-commerce project
-      </h1>
+      <main>
+        <Hero />
+      </main>
     </>
   )
 }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,19 @@
+const Hero = () => {
+  return (
+    <>
+      <div>
+        <div></div>
+        <div>
+          <div>
+            <h1></h1>
+            <h1></h1>
+            <button></button>
+          </div>
+          <div></div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Hero
