a01f13439f8021d95d315f301217fef476008311
Lasha Magaldadze <lashagiorgi420@gmail.com>
2024-04-09 16:22:06 -0700

    slider component top stories is done

--- src/components/Stories.jsx
+++ src/components/Stories.jsx
@@ -1,15 +1,30 @@
 import Title from './utils/Title'
 import { Splide, SplideSlide } from '@splidejs/react-splide'
 import '@splidejs/react-splide/css'
+import { truncate } from 'lodash'
 
 /* eslint-disable react/prop-types */
 const Stories = ({ story: { title, news } }) => {
+  const slideOptions = {
+    perPage: 4,
+    keyboard: 'global',
+    gap: '1rem',
+    pagination: false,
+    padding: '2rem',
+    breakpoints: {
+      1200: { perPage: 3 },
+      991: { perPage: 2.3 },
+      768: { perPage: 2 },
+      500: { perPage: 1.3 },
+      425: { perPage: 1 },
+    },
+  }
   return (
     <>
       <div className='nike-container mb-11'>
         <Title title={title} />
-        <div className=''>
-          <Splide>
+        <div className='mt-7'>
+          <Splide options={slideOptions}>
             {news.map((val, i) => (
               <SplideSlide key={i} className='mb-0.5'>
                 <div className='relative grid items-center gap-4 pb-2 rounded-lg shadow shadow-slate-200 ring-1 ring-slate-200'>
@@ -77,13 +92,15 @@ const Stories = ({ story: { title, news } }) => {
                       {val.title}
                     </h1>
                     <p className='text-sm text-justify lg:text-xs'>
-                      {val.text}
+                      {truncate(val.text, { length: 175 })}
                     </p>
                   </div>
                   <div className='flex items-center justify-center px-4 w-full'>
                     <a
                       href={val.url}
-                      className='w-full bg-gradient-to-b from-slate-900 to-black shadow-md shadow-black text-center text-slate-100 py-1.5'
+                      target='_blank'
+                      role='button'
+                      className='w-full bg-gradient-to-b from-slate-900 to-black shadow-md shadow-black text-center text-slate-100 py-1.5 button-theme'
                     >
                       {val.btn}
                     </a>
