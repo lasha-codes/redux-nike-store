8034886230270f1e3a29b805646c527cd39ea879
Lasha Magaldadze <lashagiorgi420@gmail.com>
2024-04-09 15:24:13 -0700

    we are done with the flex content component with dynamic and conditional styling and it does look great

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,10 @@ import {
 const App = () => {
   return (
     <>
-      <main className='flex flex-col gap-16 relative'>
+      <main className='flex flex-col gap-16 relative pb-10'>
         <Hero heroapi={heroapi} />
         <Sales endpoint={popularsales} ifExists />
-        <FlexContent endpoint={highlight} />
+        <FlexContent endpoint={highlight} ifExists />
         <Sales endpoint={toprateslaes} />
         <FlexContent endpoint={sneaker} />
       </main>
