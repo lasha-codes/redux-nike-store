72760193ac492e2ecff5b3b7c642484ff7078ff3
Lasha Magaldadze <lashagiorgi420@gmail.com>
2024-04-09 11:40:21 -0700

    products section is done and products themselves it looks great

--- src/components/Sales.jsx
+++ src/components/Sales.jsx
@@ -5,9 +5,9 @@ import Title from './utils/Title'
 const Sales = ({ endpoint: { title, items } }) => {
   return (
     <>
-      <div>
+      <div className='nike-container my-10'>
         <Title title={title} />
-        <div>
+        <div className='grid items-center justify-items-center grid-cols-4 xl:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-7 lg:gap-5 mt-7'>
           {items?.map((item, i) => {
             return <Item {...item} key={i} />
           })}
--- src/components/utils/Item.jsx
+++ src/components/utils/Item.jsx
@@ -1,9 +1,9 @@
 /* eslint-disable react/prop-types */
-const Item = ({ id, color, shadow, title, text, img, btn, rating, price }) => {
+const Item = ({ color, shadow, title, text, img, btn, rating, price }) => {
   return (
     <>
       <div
-        className={`relative bg-gradient-to-b ${color} ${shadow} grid items-center justify-items-center`}
+        className={`relative bg-gradient-to-b ${color} ${shadow} grid items-center justify-items-center rounded-xl py-4 px-5 transition-all duration-700 ease-in-out w-full hover:scale-105`}
       >
         <div className='grid items-center justify-items-center'>
           <h1 className='text-slate-200 text-xl lg:text-lg md:text-base font-medium filter drop-shadow'>
@@ -12,9 +12,9 @@ const Item = ({ id, color, shadow, title, text, img, btn, rating, price }) => {
           <p className='text-slate-200 filter drop-shadow text-base md:text-sm font-normal'>
             {text}
           </p>
-          <div className='flex items-center justify-between w-28'>
-            <div className='flex items-center bg-white/80 px-1 rounded'>
-              <h1 className='text-black text-sm'>${price}</h1>
+          <div className='flex items-center justify-between w-28 my-2'>
+            <div className='flex items-center bg-white/80 px-1 rounded blur-effect-theme'>
+              <h1 className='text-black text-sm font-medium'>${price}</h1>
             </div>
             <div>
               <h1 className='flex items-center'>
@@ -37,8 +37,11 @@ const Item = ({ id, color, shadow, title, text, img, btn, rating, price }) => {
               </h1>
             </div>
           </div>
-          <div className='flex items-center'>
-            <button type='button' className=''>
+          <div className='flex items-center gap-3'>
+            <button
+              type='button'
+              className='bg-white opacity-90 blur-effect-theme button-theme p-0.5 shadow-slate-200'
+            >
               <svg
                 xmlns='http://www.w3.org/2000/svg'
                 viewBox='0 0 24 24'
@@ -52,13 +55,20 @@ const Item = ({ id, color, shadow, title, text, img, btn, rating, price }) => {
                 />
               </svg>
             </button>
-            <button type='button' className=''>
+            <button
+              type='button'
+              className='bg-white opacity-90 blur-effect-theme button-theme px-2 py-1 shadow-slate-200 text-sm text-black'
+            >
               {btn}
             </button>
           </div>
         </div>
         <div className='flex items-center'>
-          <img src={img} alt='img/item-img' className='' />
+          <img
+            src={img}
+            alt='img/item-img'
+            className='h-36 w-64 transitions-theme hover:-rotate-12'
+          />
         </div>
       </div>
     </>
