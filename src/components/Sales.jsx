4cc97f87eb842dff813026e88176abf18314a634
Lasha Magaldadze <lashagiorgi420@gmail.com>
2024-04-09 11:48:47 -0700

    dynamic styling for our popular sales section

--- src/components/Sales.jsx
+++ src/components/Sales.jsx
@@ -2,12 +2,18 @@
 import Item from './utils/Item'
 import Title from './utils/Title'
 
-const Sales = ({ endpoint: { title, items } }) => {
+const Sales = ({ ifExists, endpoint: { title, items } }) => {
   return (
     <>
       <div className='nike-container my-10'>
         <Title title={title} />
-        <div className='grid items-center justify-items-center grid-cols-4 xl:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-7 lg:gap-5 mt-7'>
+        <div
+          className={`grid items-center justify-items-center gap-7 lg:gap-5 mt-7 ${
+            ifExists
+              ? 'grid-cols-3 xl:grid-cols-2 sm:grid-cols-1'
+              : 'grid-cols-4 xl:grid-cols-3 md:grid-cols-2 sm:grid-cols-1'
+          }`}
+        >
           {items?.map((item, i) => {
             return <Item {...item} key={i} />
           })}
