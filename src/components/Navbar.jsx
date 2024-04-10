923f22bd759304ddafcd3a40f634927084ae2826
Lasha Magaldadze <lashagiorgi420@gmail.com>
2024-04-09 17:14:27 -0700

    main design of the navbar is done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,15 @@
+import logo from '../assets/logo.png'
+
 const Navbar = () => {
   return (
     <>
-      <header>
-        <nav>
-          <div>
-            <img alt='logo/img' className='' />
+      <header className={`absolute top-7 left-0 right-0 opacity-100 z-50`}>
+        <nav className='flex items-center justify-between nike-container'>
+          <div className='flex items-center'>
+            <img src={logo} alt='logo/img' className='w-16 h-auto' />
           </div>
-          <ul>
-            <li>
+          <ul className='flex items-center justify-center gap-2'>
+            <li className='grid items-center'>
               <svg
                 xmlns='http://www.w3.org/2000/svg'
                 fill='none'
@@ -23,7 +25,7 @@ const Navbar = () => {
                 />
               </svg>
             </li>
-            <li>
+            <li className='grid items-center'>
               <svg
                 xmlns='http://www.w3.org/2000/svg'
                 fill='none'
@@ -39,7 +41,32 @@ const Navbar = () => {
                 />
               </svg>
             </li>
-            <li></li>
+            <li className='grid items-center'>
+              <button
+                type='button'
+                className='border-none outline-none active:scale-110 transition-all duration-300 relative'
+              >
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  fill='none'
+                  viewBox='0 0 24 24'
+                  strokeWidth={1.5}
+                  stroke='currentColor'
+                  className='icon-style'
+                >
+                  <path
+                    strokeLinecap='round'
+                    strokeLinejoin='round'
+                    d='M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z'
+                  />
+                </svg>
+                <div
+                  className={`absolute top-4 right-1 bg-white text-slate-900 shadow shadow-slate-100 w-4 h-4 text-[0.65rem] leading-tight font-medium rounded-full flex items-center justify-center cursor-pointer hover:scale-110 duration-300`}
+                >
+                  0
+                </div>
+              </button>
+            </li>
           </ul>
         </nav>
       </header>
