e0dd261ab14c1b539f1d15271dad410dbdd6c47b
Lasha Magaldadze <lashagiorgi420@gmail.com>
2024-04-09 20:23:26 -0700

    lots of styling going on i must say

--- src/components/cart/CartCount.jsx
+++ src/components/cart/CartCount.jsx
@@ -1,12 +1,54 @@
 const CartCount = () => {
   return (
     <>
-      <div>
-        <div>
-          <div></div>
-          <div></div>
+      <div className='bg-white h-11 flex items-center justify-between px-3 sticky top-0 left-0 right-0 w-full'>
+        <div className='flex items-center gap-3'>
+          <div className='grid items-center cursor-pointer'>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              fill='none'
+              viewBox='0 0 24 24'
+              strokeWidth={1.5}
+              stroke='currentColor'
+              className='w-5 h-5'
+            >
+              <path
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                d='m18.75 4.5-7.5 7.5 7.5 7.5m-6-15L5.25 12l7.5 7.5'
+              />
+            </svg>
+          </div>
+          <div className='grid items-center text-slate-900 hover:text-orange-500 stroke-[2] transition-all duration-300'>
+            <h1 className='text-base font-medium text-slate-900 flex items-center gap-2'>
+              Your Cart
+              <span className='bg-theme-cart rounded px-1 py-0.5 text-slate-100 font-normal text-xs'>
+                (items)
+              </span>
+            </h1>
+          </div>
         </div>
-        <div></div>
+        <div className='flex items-center'>
+          <button
+            type='button'
+            className='rounded bg-theme-cart active:scale-90 p-0.5'
+          >
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              fill='none'
+              viewBox='0 0 24 24'
+              strokeWidth={1.5}
+              stroke='currentColor'
+              className='w-5 h-5 text-white stroke-[2]'
+            >
+              <path
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                d='M6 18 18 6M6 6l12 12'
+              />
+            </svg>
+          </button>
+        </div>
       </div>
     </>
   )
--- src/components/cart/CartEmpty.jsx
+++ src/components/cart/CartEmpty.jsx
@@ -1,5 +1,37 @@
+import emptyBag from '../../assets/emptybag.png'
+
 const CartEmpty = () => {
-  return <div>CartEmpty</div>
+  return (
+    <>
+      <div className='flex items-center justify-center flex-col h-screen px-11 text-center gap-7'>
+        <img
+          src={emptyBag}
+          alt='emptybag/img'
+          className='w-40 lg:w-36 sm:w-27 h-auto object-fill transition-all duration-300 hover:scale-110'
+        />
+        <button
+          type='button'
+          className='button-theme bg-gradient-to-b from-amber-500 to-orange-500 shadow-lg shadow-orange-500 flex items-center justify-center text-slate-900 py-2 gap-3 text-sm px-5 font-semibold active:scale-110'
+        >
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 24 24'
+            strokeWidth={1.5}
+            stroke='currentColor'
+            className='w-5 h-5 text-slate-900'
+          >
+            <path
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              d='M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18'
+            />
+          </svg>
+          <span className=''>Back To Nike Store</span>
+        </button>
+      </div>
+    </>
+  )
 }
 
 export default CartEmpty
