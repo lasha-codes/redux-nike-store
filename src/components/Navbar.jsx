8754e8442989b04a10d131cd8ad02b91650ab9cd
Lasha Magaldadze <lashagiorgi420@gmail.com>
2024-04-09 17:32:45 -0700

    very good looking navbar with come in animation when scrollY > 30

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,40 @@
+import { useEffect, useState } from 'react'
 import logo from '../assets/logo.png'
 
 const Navbar = () => {
+  const [navState, setNavState] = useState(false)
+
+  const onNavScroll = () => {
+    if (window.scrollY > 30) {
+      setNavState(true)
+    } else {
+      setNavState(false)
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', onNavScroll)
+    return () => {
+      window.removeEventListener('scroll', onNavScroll)
+    }
+  }, [])
+
   return (
     <>
-      <header className={`absolute top-7 left-0 right-0 opacity-100 z-50`}>
+      <header
+        className={`absolute top-7 left-0 right-0 opacity-100 z-[200] transition-all duration-300 ${
+          !navState
+            ? 'opacity-100 z-50'
+            : '!fixed !top-0 left-0 right-0 h-[9vh] flex items-center justify-center opacity-100 z-50 blur-effect-theme'
+        }`}
+      >
         <nav className='flex items-center justify-between nike-container'>
           <div className='flex items-center'>
-            <img src={logo} alt='logo/img' className='w-16 h-auto' />
+            <img
+              src={logo}
+              alt='logo/img'
+              className={`w-16 h-auto ${navState && 'filter brightness-0'}`}
+            />
           </div>
           <ul className='flex items-center justify-center gap-2'>
             <li className='grid items-center'>
@@ -16,7 +44,9 @@ const Navbar = () => {
                 viewBox='0 0 24 24'
                 strokeWidth={1.5}
                 stroke='currentColor'
-                className='icon-style'
+                className={`icon-style transition-all duration-300 ${
+                  navState && 'text-slate-900'
+                }`}
               >
                 <path
                   strokeLinecap='round'
@@ -32,7 +62,9 @@ const Navbar = () => {
                 viewBox='0 0 24 24'
                 strokeWidth={1.5}
                 stroke='currentColor'
-                className='icon-style'
+                className={`icon-style transition-all duration-300 ${
+                  navState && 'text-slate-900'
+                }`}
               >
                 <path
                   strokeLinecap='round'
@@ -52,7 +84,9 @@ const Navbar = () => {
                   viewBox='0 0 24 24'
                   strokeWidth={1.5}
                   stroke='currentColor'
-                  className='icon-style'
+                  className={`icon-style transition-all duration-300 ${
+                    navState && 'text-slate-900'
+                  }`}
                 >
                   <path
                     strokeLinecap='round'
@@ -61,7 +95,11 @@ const Navbar = () => {
                   />
                 </svg>
                 <div
-                  className={`absolute top-4 right-1 bg-white text-slate-900 shadow shadow-slate-100 w-4 h-4 text-[0.65rem] leading-tight font-medium rounded-full flex items-center justify-center cursor-pointer hover:scale-110 duration-300`}
+                  className={`absolute top-4 right-1 shadow w-4 h-4 text-[0.65rem] leading-tight font-medium rounded-full flex items-center justify-center cursor-pointer hover:scale-110 duration-300 ${
+                    navState
+                      ? 'bg-slate-900 text-slate-100 shadow-slate-900'
+                      : 'bg-slate-100 text-slate-900 shadow-slate-100'
+                  }`}
                 >
                   0
                 </div>
